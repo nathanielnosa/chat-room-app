7d5b98cbddeedee2d5bc07b17f98efd15d019e90
nathaniel nosa <nathanielnosa@gmail.com>
2021-12-09 21:49:19 +0100

    pushing to heroku

--- nchat/settings.py
+++ nchat/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y-&g7a%)__rof7b*q%4^6iilsf!jq31^_2o82*vky3!fror@i6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','nchats.herokuapp.com']
 
 
 # Application definition
@@ -75,11 +75,31 @@ WSGI_APPLICATION = 'nchat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'd894tqkikalkh4',
+
+        'USER': 'yayxxwljlvemxi',
+
+        'PASSWORD': '9acdbd8282ea170ff34e1a3a28eb4da792b352a12958da29aaeb992d6f03d387',
+
+        'HOST': 'ec2-3-89-214-80.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
@@ -125,6 +145,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'nchat/static')
 ]
 
+django_heroku.settings(locals())
 
 # media files
 # media folder

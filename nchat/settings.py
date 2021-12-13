aafbf9951746219742bec4b396e38ff282644cfb
nathaniel nosa <nathanielnosa@gmail.com>
2021-12-13 11:49:15 +0100

    check later

--- nchat/settings.py
+++ nchat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y-&g7a%)__rof7b*q%4^6iilsf!jq31^_2o82*vky3!fror@i6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'nchats.herokuapp.com']
 
